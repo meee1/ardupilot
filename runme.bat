2173b9d6c1d809c04085ae2be1994bbe6db3b0e4
Michael Oborne <mich146@hotmail.com>
2019-04-18 23:55:36 +0800

    libs

--- runme.bat
+++ runme.bat
@@ -5,7 +5,7 @@ rem pause
 
 cl arducopter\arducopter.cpp /c  /showIncludes  -DPSNIP_BUILTIN_EMULATE_NATIVE /FI ..\portable-snippets\builtin\builtin.h -I.. -I..\portable-snippets -I..\pthread /GR /MP  -Ibuild\sitl  -Ibuild\sitl\libraries\GCS_MAVLink   "-Ilibraries"       "-Ilibraries/GCS_MAVLink"      "-I."       "-Ilibraries"       "-Ilibraries/AP_Common/missing"  -I../libunistd/unistd -DCONFIG_HAL_BOARD=HAL_BOARD_SITL -DCONFIG_HAL_BOARD_SUBTYPE=HAL_BOARD_SUBTYPE_NONE  /link /LIBPATH:.;..
 
-cl arducopter\*.cpp    /w /GR /MP  -DPSNIP_BUILTIN_EMULATE_NATIVE /FI ..\portable-snippets\builtin\builtin.h -I..\portable-snippets  -I..\pthread  -Ibuild\sitl  -Ibuild\sitl\libraries\GCS_MAVLink   "-Ilibraries"       "-Ilibraries/GCS_MAVLink"      "-I."       "-Ilibraries"       "-Ilibraries/AP_Common/missing"  -I../libunistd/unistd -DCONFIG_HAL_BOARD=HAL_BOARD_SITL -DCONFIG_HAL_BOARD_SUBTYPE=HAL_BOARD_SUBTYPE_NONE
+cl arducopter\*.cpp  libcpp\*.lib /LD  /w /GR /MP  -DPSNIP_BUILTIN_EMULATE_NATIVE /FI ..\portable-snippets\builtin\builtin.h -I..\portable-snippets  -I..\pthread  -Ibuild\sitl  -Ibuild\sitl\libraries\GCS_MAVLink   "-Ilibraries"       "-Ilibraries/GCS_MAVLink"      "-I."       "-Ilibraries"       "-Ilibraries/AP_Common/missing"  -I../libunistd/unistd -DCONFIG_HAL_BOARD=HAL_BOARD_SITL -DCONFIG_HAL_BOARD_SUBTYPE=HAL_BOARD_SUBTYPE_NONE /link /LIBPATH:.;..;libcpp
 
 
 rem cl libraries\AP_HAL_Empty\HAL_Empty_Class.cpp    /std:c++latest -I.. -I..\libunistd\unistd /w /GR /MP   -Ibuild\sitl  -Ibuild\sitl\libraries\GCS_MAVLink   "-Ilibraries"       "-Ilibraries/GCS_MAVLink"      "-I."       "-I../../libraries"       "-I../../libraries/AP_Common/missing" -DCONFIG_HAL_BOARD=HAL_BOARD_SITL -DCONFIG_HAL_BOARD_SUBTYPE=HAL_BOARD_SUBTYPE_NONE /link /LIBPATH:.;..
@@ -25,7 +25,8 @@ pause
 for /F %%i in ('dir /b libraries') do (
 mkdir libcpp
 mkdir libcpp\%%i
-cl libraries\%%i\*.cpp  /std:c++latest /w  /MP /Folibcpp\%%i\ -DPSNIP_BUILTIN_EMULATE_NATIVE /FI ..\portable-snippets\builtin\builtin.h -I.. -I..\portable-snippets -I..\pthread  -Ibuild\sitl  -Ibuild\sitl\libraries\GCS_MAVLink   "-Ilibraries"       "-Ilibraries/GCS_MAVLink"      "-I."       "-Ilibraries"       "-Ilibraries/AP_Common/missing" -I../libunistd/unistd -DCONFIG_HAL_BOARD=HAL_BOARD_SITL -DCONFIG_HAL_BOARD_SUBTYPE=HAL_BOARD_SUBTYPE_NONE /link /LIBPATH:.;..
+cl libraries\%%i\*.cpp /c /LD /w  /MP /Folibcpp\%%i\ -DPSNIP_BUILTIN_EMULATE_NATIVE /FI ..\portable-snippets\builtin\builtin.h /FI ..\portable-snippets\random\random.h -I.. -I..\portable-snippets -I..\pthread  -Ibuild\sitl  -Ibuild\sitl\libraries\GCS_MAVLink   "-Ilibraries"       "-Ilibraries/GCS_MAVLink"      "-I."       "-Ilibraries"       "-Ilibraries/AP_Common/missing" -I../libunistd/unistd -DCONFIG_HAL_BOARD=HAL_BOARD_SITL -DCONFIG_HAL_BOARD_SUBTYPE=HAL_BOARD_SUBTYPE_NONE
+lib libcpp\%%i\*.obj /out:libcpp\%%i.lib
 )
 
 for /F %%i in ('dir /s /b libraries\*.cpp') do (
