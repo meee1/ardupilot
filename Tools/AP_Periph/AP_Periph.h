b5c402fcb9ca90262e52b714a55a284ddae41373
Michael Oborne <mich146@hotmail.com>
2020-01-20 10:54:30 +0800

    gcs_dummy

--- Tools/AP_Periph/AP_Periph.h
+++ Tools/AP_Periph/AP_Periph.h
@@ -1,5 +1,6 @@
 #include <AP_HAL/AP_HAL.h>
 #include <AP_Param/AP_Param.h>
+#include <GCS_MAVLink/GCS_Dummy.h>
 #include <AP_GPS/AP_GPS.h>
 #include <AP_Compass/AP_Compass.h>
 #include <AP_Baro/AP_Baro.h>
@@ -33,65 +34,6 @@
   app descriptor compatible with MissionPlanner
  */
 extern const struct app_descriptor app_descriptor;
-
-/**
- * @brief   CAN transmission frame.
- * @note    Accessing the frame data as word16 or word32 is not portable because
- *          machine data endianness, it can be still useful for a quick filling.
- */
-typedef struct {
-  struct {
-    uint8_t                 DLC:4;          /**< @brief Data length.        */
-    uint8_t                 RTR:1;          /**< @brief Frame type.         */
-    uint8_t                 IDE:1;          /**< @brief Identifier type.    */
-  };
-  union {
-    struct {
-      uint32_t              SID:11;         /**< @brief Standard identifier.*/
-    };
-    struct {
-      uint32_t              EID:29;         /**< @brief Extended identifier.*/
-    };
-  };
-  union {
-    uint8_t                 data8[8];       /**< @brief Frame data.         */
-    uint16_t                data16[4];      /**< @brief Frame data.         */
-    uint32_t                data32[2];      /**< @brief Frame data.         */
-    uint64_t                data64[1];      /**< @brief Frame data.         */
-  };
-} CANTxFrame;
-
-
-/**
- * @brief   CAN received frame.
- * @note    Accessing the frame data as word16 or word32 is not portable because
- *          machine data endianness, it can be still useful for a quick filling.
- */
-typedef struct {
-  struct {
-    uint8_t                 FMI;            /**< @brief Filter id.          */
-    uint16_t                TIME;           /**< @brief Time stamp.         */
-  };
-  struct {
-    uint8_t                 DLC:4;          /**< @brief Data length.        */
-    uint8_t                 RTR:1;          /**< @brief Frame type.         */
-    uint8_t                 IDE:1;          /**< @brief Identifier type.    */
-  };
-  union {
-    struct {
-      uint32_t              SID:11;         /**< @brief Standard identifier.*/
-    };
-    struct {
-      uint32_t              EID:29;         /**< @brief Extended identifier.*/
-    };
-  };
-  union {
-    uint8_t                 data8[8];       /**< @brief Frame data.         */
-    uint16_t                data16[4];      /**< @brief Frame data.         */
-    uint32_t                data32[2];      /**< @brief Frame data.         */
-    uint64_t                data64[1];      /**< @brief Frame data.         */
-  };
-} CANRxFrame;
 
 class AP_Periph_FW {
 public:
@@ -133,6 +75,8 @@ public:
     AP_HAL::AnalogSource *_adc1;
     AP_HAL::AnalogSource *_adc2;
     AP_HAL::AnalogSource *_adc3;
+
+    GCS_Dummy _gcs;
   
 
     // Inertial Navigation
