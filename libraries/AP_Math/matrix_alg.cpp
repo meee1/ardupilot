23607ab4a3c355b76446c647900426c25cfec71b
Michael Oborne <mich146@hotmail.com>
2020-05-17 11:22:58 +0800

    Update matrix_alg.cpp

--- libraries/AP_Math/matrix_alg.cpp
+++ libraries/AP_Math/matrix_alg.cpp
@@ -284,12 +284,7 @@ bool inverse4x4(float m[],float invOut[])
     float inv[16], det;
     uint8_t i;
 
-#if CONFIG_HAL_BOARD == HAL_BOARD_SITL
-    int old = fedisableexcept(FE_OVERFLOW);
-    if (old < 0) {
-        hal.console->printf("inverse4x4(): warning: error on disabling FE_OVERFLOW floating point exception\n");
-    }
-#endif
+
 
     inv[0] = m[5]  * m[10] * m[15] -
     m[5]  * m[11] * m[14] -
@@ -405,11 +400,7 @@ bool inverse4x4(float m[],float invOut[])
 
     det = m[0] * inv[0] + m[1] * inv[4] + m[2] * inv[8] + m[3] * inv[12];
 
-#if CONFIG_HAL_BOARD == HAL_BOARD_SITL
-    if (old >= 0 && feenableexcept(old) < 0) {
-        hal.console->printf("inverse4x4(): warning: error on restoring floating exception mask\n");
-    }
-#endif
+
 
     if (is_zero(det) || isinf(det)){
         return false;
