4236116d8367ac673e58c38ec41cf7faa63c7a05
Siddharth Purohit <siddharthbharatpurohit@gmail.com>
2020-11-08 17:47:35 +0530

    AP_CANManager: fix slcan only operations

--- libraries/AP_CANManager/AP_SLCANIface.cpp
+++ libraries/AP_CANManager/AP_SLCANIface.cpp
@@ -626,6 +626,10 @@ bool SLCAN::CANIface::select(bool &read, bool &write, const AP_HAL::CANFrame* co
         return ret;
     }
 
+    if (!ret) {
+        read = false;
+    }
+
     // if under passthrough, we only do send when can_iface also allows it
     if (_port->available_locked(_serial_lock_key) || rx_queue_.available()) {
         // allow for receiving messages over slcan
